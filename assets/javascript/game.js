605e81c4a1ac58500c3d1c5715c91c6fef17dae4
Jing Xie <xiejing418@gmail.com>
2019-04-18 01:10:55 -0700

    first update

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var character=[{name:"Darth_Vader",image:"assets/images/darthVader.png", HP:160 },
+               {name:"BB8", image:"assets/images/bb8.png", HP:180 },
+               {name:"Yoda", image:"assets/images/yoda.png", HP:120 },
+               {name:"Rey", image:"assets/images/bb8.png", HP:150 }]
+
+
+$(document).ready(function(){
+    $(".img-frame-cap").on("click",function(){
+        $(".img-frame-cap").hide();
+        var characterID = $(this).attr("data-id");
+    
+        
+
+
+
+
+    })
+
+})
+
+console.log("check");
+var characterID = $(this).attr("data-id");
+     
