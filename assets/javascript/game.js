1801d4e2dccb4132d6c2dfe0d294f5a74ffdb949
Jing Xie <xiejing418@gmail.com>
2019-04-19 13:43:48 -0700

    name&hp update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,17 @@
-var character = [{ name: "darthVader", image: "assets/images/darthVader.png", HP: 160 },
-{ name: "bb8", image: "assets/images/bb8.png", HP: 180 },
-{ name: "yoda", image: "assets/images/yoda.png", HP: 120 },
-{ name: "rey", image: "assets/images/rey.png", HP: 150 }];
+var character = [{ name: "DarthVader", image: "assets/images/darthVader.png", HP: 160 },
+{ name: "BB8", image: "assets/images/bb8.png", HP: 180 },
+{ name: "Yoda", image: "assets/images/yoda.png", HP: 120 },
+{ name: "Rey", image: "assets/images/rey.png", HP: 150 }];
 
-var defenderImage = ["","",""];
+var characterImage = "";
+var characterName = "";
+var hpLevel = "";
 var t = 0;
+var defenderImage = ["","",""];
+var defenderNmae = ["","",""];
+var defenderHP = ["","",""];
+
+
 
 
 // click one character
@@ -15,47 +22,65 @@ $(document).ready(function () {
     $(".firstSection").remove();
     // get data-id for the clicked character
     var characterID = $(this).attr("data-id");
+
     // get image link for the clicked character
     for (i = 0; i < character.length; i++) {
       if (characterID === character[i].name) {
-        var characterImage = character[i].image;
+          characterImage = character[i].image;
+          characterName = character[i].name;
+          hpLevel = character[i].HP;
+
       }
+      // git image link for defender characters
       else {
         defenderImage[t] = character[i].image;
+        defenderNmae[t] = character[i].name;
+        defenderHP[t]= character[i].HP;
         t++;
+
       }
 
     }
-    console.log(defenderImage);
-    // add image link for the clicked character to your character section
+  
+    // add image link, name & hp level for the clicked character to your character section
+    $(".img-frame-cap2").show();
+
     $(".img-frame-cap2 img").attr({
       "src": characterImage,
       "alt": characterID
-
     });
-    $(".img-frame-cap2").show();
+   
+   $(".img-frame-cap2 .CharacterName").html(characterName);
+   $(".img-frame-cap2 .HP").html(hpLevel);
 
+
+   // add image link, name & hp level for defender characters to your defender section
     $("section").prepend(hideThirdSection);
 
 
     $(".img-frame-cap3-1 img").attr({
       "src": defenderImage[0],
       "alt": characterID
-
     });
+
+    $(".img-frame-cap3-1 .CharacterName").html(defenderNmae[0]);
+    $(".img-frame-cap3-1 .HP").html(defenderHP[0]);
 
     $(".img-frame-cap3-2 img").attr({
       "src": defenderImage[1],
       "alt": characterID
-
     });
+
+    $(".img-frame-cap3-2 .CharacterName").html(defenderNmae[1]);
+    $(".img-frame-cap3-2 .HP").html(defenderHP[1]);
 
     $(".img-frame-cap3-3 img").attr({
       "src": defenderImage[2],
       "alt": characterID
-
     });
    
+    $(".img-frame-cap3-3 .CharacterName").html(defenderNmae[2]);
+    $(".img-frame-cap3-3 .HP").html(defenderHP[2]);
 
     
     
