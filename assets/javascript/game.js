5a4130f009d2ad37e2e5551816c8ef5ad1fb8267
Jing Xie <xiejing418@gmail.com>
2019-04-19 12:18:04 -0700

    section update2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var character = [{ name: "darthVader", image: "assets/images/darthVader.png", HP
 { name: "yoda", image: "assets/images/yoda.png", HP: 120 },
 { name: "rey", image: "assets/images/rey.png", HP: 150 }];
 
-var defenderImage = ["defenderImage1", "defenderImage2", "defenderImage3"];
+var defenderImage = [];
 
 
 // click one character
@@ -20,10 +20,11 @@ $(document).ready(function () {
         var characterImage = character[i].image;
       }
       else {
-        defenderImage = character[i].image;
+       defenderImage = character[i].image;
       }
-    console.log(defenderImage);
+
     }
+    console.log(defenderImage);
     // add image link for the clicked character to your character section
     $(".img-frame-cap2 img").attr({
       "src": characterImage,
@@ -32,25 +33,18 @@ $(document).ready(function () {
     });
     $(".img-frame-cap2").show();
 
-    $(".img-frame-cap3-1 img").attr({
-      "src": defenderImage[0],
-      "alt": characterID
-
-    });
-
-    $(".img-frame-cap3-2 img").attr({
-      "src": defenderImage[1],
-      "alt": characterID
-
-    });
-
-    $(".img-frame-cap3-3 img").attr({
-      "src": defenderImage[2],
-      "alt": characterID
-
-    });
     $("section").prepend(hideThirdSection);
 
+
+    $(".img-frame-cap3-1 img").attr({
+      "src": defenderImage,
+      "alt": characterID
+
+    });
+   
+
+    
+    
 
 
 
