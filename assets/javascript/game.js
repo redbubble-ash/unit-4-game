af00d072a618a3b993a801429cbb97a9144fdcf6
Jing Xie <xiejing418@gmail.com>
2019-04-19 00:40:29 -0700

    update my character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,38 @@
-var character=[{name:"Darth_Vader",image:"assets/images/darthVader.png", HP:160 },
-               {name:"BB8", image:"assets/images/bb8.png", HP:180 },
-               {name:"Yoda", image:"assets/images/yoda.png", HP:120 },
-               {name:"Rey", image:"assets/images/bb8.png", HP:150 }]
+var character=[{name:"darthVader",image:"assets/images/darthVader.png", HP:160 },
+               {name:"bb8", image:"assets/images/bb8.png", HP:180 },
+               {name:"yoda", image:"assets/images/yoda.png", HP:120 },
+               {name:"rey", image:"assets/images/rey.png", HP:150 }]
 
 
+// click one character
 $(document).ready(function(){
+    $(".img-frame-cap2").hide();
     $(".img-frame-cap").on("click",function(){
         $(".firstSection").remove();
+        // get data-id for the clicked character
         var characterID = $(this).attr("data-id");
+        // get image link for the clicked character
+        for (i=0;i<character.length;i++){
+          if (characterID === character[i].name){
+              var characterImage = character[i].image;
+            }
+          }
+        // add image link for the clicked character to your character section
+        $(".img-frame-cap2 img").attr({
+            "src": characterImage,
+            "alt": characterID
+    
+        });
+        $(".img-frame-cap2").show();
+    });
+
     
         
 
 
 
 
-    })
+    
 
 })
 
