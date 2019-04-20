b58a2dc9158974cdde57c5b7079d17ffa7c9fae6
Jing Xie <xiejing418@gmail.com>
2019-04-19 19:48:10 -0700

    update reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-var character = [{ name: "DarthVader", image: "assets/images/darthVader.png", HP: 160, attackPower: 20, counterattackPower: 30},
+var character = [{ name: "DarthVader", image: "assets/images/darthVader.png", HP: 160, attackPower: 20, counterattackPower: 30 },
 { name: "BB8", image: "assets/images/bb8.png", HP: 180, attackPower: 30, counterattackPower: 40 },
 { name: "Yoda", image: "assets/images/yoda.png", HP: 120, attackPower: 10, counterattackPower: 20 },
-{ name: "Rey", image: "assets/images/rey.png", HP: 150, attackPower:10, counterattackPower:25 }];
+{ name: "Rey", image: "assets/images/rey.png", HP: 150, attackPower: 10, counterattackPower: 25 }];
 
 var characterImage = "";
 var characterName = "";
@@ -9,11 +9,11 @@ var hpLevel = "";
 var attackPower = "";
 var newAttackPower = 0;
 var t = 0;
-var defenderImage = ["","",""];
-var defenderNmae = ["","",""];
-var defenderHP = ["","",""];
-var defenderAttackPower = ["","",""];
-var defenderCounterAttackPower = ["","",""];
+var defenderImage = ["", "", ""];
+var defenderNmae = ["", "", ""];
+var defenderHP = ["", "", ""];
+var defenderAttackPower = ["", "", ""];
+var defenderCounterAttackPower = ["", "", ""];
 
 
 
@@ -21,6 +21,7 @@ $(document).ready(function () {
   $(".img-frame-cap2").hide();
   var hideThirdSection = $(".thirdSection").detach();
   $(".img-frame-cap4").hide();
+  $(".restart").hide();
 
   // choose a fighter by clicking an image
   $(".img-frame-cap").on("click", function () {
@@ -31,17 +32,17 @@ $(document).ready(function () {
     // get image link for the clicked character
     for (i = 0; i < character.length; i++) {
       if (characterID === character[i].name) {
-          characterImage = character[i].image;
-          characterName = character[i].name;
-          attackPower = character[i].attackPower;
-          hpLevel = character[i].HP;
-          
+        characterImage = character[i].image;
+        characterName = character[i].name;
+        attackPower = character[i].attackPower;
+        hpLevel = character[i].HP;
+
       }
       // git image link for defender characters
       else {
         defenderImage[t] = character[i].image;
         defenderNmae[t] = character[i].name;
-        defenderHP[t]= character[i].HP;
+        defenderHP[t] = character[i].HP;
         defenderAttackPower[t] = character[i].attackPower;
         defenderCounterAttackPower[t] = character[i].counterattackPower;
         t++;
@@ -49,7 +50,7 @@ $(document).ready(function () {
       }
 
     }
-  
+
     // add image link, name & hp level for the clicked character to your character section
     $(".img-frame-cap2").show();
 
@@ -57,19 +58,19 @@ $(document).ready(function () {
       "src": characterImage,
       "alt": characterID
     });
-   
-   $(".img-frame-cap2 .CharacterName").html(characterName);
-   $(".img-frame-cap2 .HP").html(hpLevel);
+
+    $(".img-frame-cap2 .CharacterName").html(characterName);
+    $(".img-frame-cap2 .HP").html(hpLevel);
 
 
-   // add image link, name & hp level for defender characters to your defender section
+    // add image link, name & hp level for defender characters to your defender section
     $("section").prepend(hideThirdSection);
 
     $(".img-frame-cap3-1 img").attr("src", defenderImage[0]);
     $(".img-frame-cap3-1 .CharacterName").html(defenderNmae[0]);
     $(".img-frame-cap3-1 .HP").html(defenderHP[0]);
 
-    $(".img-frame-cap3-2 img").attr( "src", defenderImage[1],);
+    $(".img-frame-cap3-2 img").attr("src", defenderImage[1]);
     $(".img-frame-cap3-2 .CharacterName").html(defenderNmae[1]);
     $(".img-frame-cap3-2 .HP").html(defenderHP[1]);
 
@@ -78,62 +79,85 @@ $(document).ready(function () {
     $(".img-frame-cap3-3 .HP").html(defenderHP[2]);
 
     // choose defender by clicking an image from enemies available list
-    $(".img-frame-cap3-1").on("click", function(){
+    $(".img-frame-cap3-1").on("click", function () {
       $(".img-frame-cap4 img").attr("src", defenderImage[0]);
       $(".img-frame-cap4 .CharacterName").html(defenderNmae[0]);
       $(".img-frame-cap4 .HP").html(defenderHP[0]);
       $(".img-frame-cap3-1").detach();
       $(".img-frame-cap4").show();
-
-      $("button").on("click", function(){
+      //  hp level decreases when attack 
+      $("button").on("click", function () {
         hpLevel -= defenderCounterAttackPower[0];
         newAttackPower += attackPower;
         defenderHP[0] -= newAttackPower;
         $(".img-frame-cap2 .HP").html(hpLevel);
-        $(".img-frame-cap4 .HP").html(defenderHP[0]);    
+        $(".img-frame-cap4 .HP").html(defenderHP[0]);
+        if (hpLevel <= 0) {
+          $("p").html("You been defeated! Game is over!");
+          $(".restart").show();
+          $(".restart").on("click",restart());
+        }
       })
-    
+
     })
 
-    $(".img-frame-cap3-2").on("click", function(){
+    $(".img-frame-cap3-2").on("click", function () {
       $(".img-frame-cap4 img").attr("src", defenderImage[1]);
       $(".img-frame-cap4 .CharacterName").html(defenderNmae[1]);
       $(".img-frame-cap4 .HP").html(defenderHP[1]);
       $(".img-frame-cap3-2").detach();
       $(".img-frame-cap4").show();
 
-      $("button").on("click", function(){
+      $("button").on("click", function () {
         hpLevel -= defenderCounterAttackPower[1];
         newAttackPower += attackPower;
         defenderHP[1] -= newAttackPower;
         $(".img-frame-cap2 .HP").html(hpLevel);
-        $(".img-frame-cap4 .HP").html(defenderHP[1]);    
+        $(".img-frame-cap4 .HP").html(defenderHP[1]);
       })
     })
 
-    $(".img-frame-cap3-3").on("click", function(){
+    $(".img-frame-cap3-3").on("click", function () {
       $(".img-frame-cap4 img").attr("src", defenderImage[2]);
       $(".img-frame-cap4 .CharacterName").html(defenderNmae[2]);
       $(".img-frame-cap4 .HP").html(defenderHP[2]);
       $(".img-frame-cap3-3").detach();
       $(".img-frame-cap4").show();
 
-      $("button").on("click", function(){
+      $("button").on("click", function () {
         hpLevel -= defenderCounterAttackPower[2];
         newAttackPower += attackPower;
         defenderHP[2] -= newAttackPower;
         $(".img-frame-cap2 .HP").html(hpLevel);
-        $(".img-frame-cap4 .HP").html(defenderHP[2]);    
+        $(".img-frame-cap4 .HP").html(defenderHP[2]);
       })
     })
-  
-  
+
+
+
 
   });
 
+  function restart() {
+    $(".img-frame-cap2").hide();
+    $(".thirdSection").detach();
+    $(".img-frame-cap4").hide();
+    $(".restart").hide();
+    characterImage = "";
+    characterName = "";
+    hpLevel = "";
+    attackPower = "";
+    newAttackPower = 0;
+    t = 0;
+    defenderImage = ["", "", ""];
+    defenderNmae = ["", "", ""];
+    defenderHP = ["", "", ""];
+    defenderAttackPower = ["", "", ""];
+    defenderCounterAttackPower = ["", "", ""];
 
-  
-  
+  }
+
+
 
 
 
